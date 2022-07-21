046609186483d31745aeea5c86cf6c051b362144
Rafly Rafsanjani <38741998+raflyrafsss@users.noreply.github.com>
2022-07-21 13:40:54 +0700

    add: component contact section

---
+++ src/components/contact/contact-section.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "../ui";
+
+const ContactSection = () => {
+  return (
+    <section id="contact">
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum
+          eius reprehenderit autem excepturi officia soluta, veniam vitae
+          tempora? Repellat sapiente optio maxime debitis natus dolores cum
+          voluptatibus? Neque, aspernatur!
+        </p>
+      </Container>
+    </section>
+  );
+};
+
+export default ContactSection;
