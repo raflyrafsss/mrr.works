ccde62c1bfd99902f1ad77b2847c350c3ad7c9fc
Rafly Rafsanjani <38741998+raflyrafsss@users.noreply.github.com>
2022-07-21 13:45:49 +0700

    add: global navigation, nav-menu, nav-social-menu

---
+++ src/components/layout/navigation.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import NextLink from "next/link";
+import { Link } from "~/components/ui";
+import { IoLogoGithub, IoMenu } from "react-icons/io5";
+import NavigationMenu from "./navigation-menu";
+import NavigationSocialMenu from "./navigation-social-menu";
+
+const Navigation = () => {
+  return (
+    <nav id="nav" className="flex flex-col lg:flex-row">
+      <div className="w-full flex items-center justify-between">
+        <div id="nav-logo" className="">
+          <Link href="/">MRR</Link>
+        </div>
+        <div className="block lg:hidden">
+          <button className="w-8 h-8 inline-flex items-center justify-center text-xl transition-transform active:scale-95">
+            <IoMenu />
+          </button>
+        </div>
+      </div>
+
+      <div className="w-full flex items-center text-center justify-center">
+        <NavigationMenu />
+      </div>
+
+      <div className="w-full flex items-center justify-center lg:justify-end">
+        <NavigationSocialMenu />
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
