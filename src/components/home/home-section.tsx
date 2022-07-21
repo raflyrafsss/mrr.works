18cc605cd3be0160460959949f5df55cc5254c40
Rafly Rafsanjani <38741998+raflyrafsss@users.noreply.github.com>
2022-07-21 13:41:12 +0700

    add: component home section

---
+++ src/components/home/home-section.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ButtonLink, Container } from "../ui";
+
+interface HomeSectionProps {
+  className?: string;
+}
+
+const HomeSection: React.FC<HomeSectionProps> = ({ className }) => {
+  return (
+    <section id="home" className="py-12">
+      <Container>
+        <h6 className="font-semibold">SOFTWARE ENGINEER</h6>
+        <h1 className="font-bold text-4xl">MUHAMMAD RAFLY RAFSANJANI</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
+          culpa.
+        </p>
+        <ButtonLink href="/">DOWNLOAD CV</ButtonLink>
+      </Container>
+    </section>
+  );
+};
+
+export default HomeSection;
