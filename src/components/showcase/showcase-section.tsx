7fec106272023aeb113848a241ab66505880cfb9
Rafly Rafsanjani <38741998+raflyrafsss@users.noreply.github.com>
2022-07-21 13:42:39 +0700

    add: component showcase section

---
+++ src/components/showcase/showcase-section.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "../ui";
+
+const ShowcaseSection = () => {
+  return (
+    <section id="showcase">
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus earum
+          eius reprehenderit autem excepturi officia soluta, veniam vitae
+          tempora? Repellat sapiente optio maxime debitis natus dolores cum
+          voluptatibus? Neque, aspernatur!
+        </p>
+      </Container>
+    </section>
+  );
+};
+
+export default ShowcaseSection;
